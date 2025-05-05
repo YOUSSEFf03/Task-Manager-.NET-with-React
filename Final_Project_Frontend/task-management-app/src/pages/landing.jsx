6ce509e5570882614ff59cfa35b654625e5b6444
Tony-Katrib <katribtony4@gmail.com>
2025-05-05 13:11:48 +0300

    Adding Header (H) and Button components and styles (global style and components styles)

---
+++ Final_Project_Frontend/task-management-app/src/pages/landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'react-router-dom';
+import '../styles/landing.css';
+import H from '../components/H.jsx';
+import Button from '../components/Button.jsx';
+import Footer from '../components/Footer.jsx';
+
+export default function Landing() {
+    return (
+        <div>
+            <div className='hero'>
+                <header>
+
+                </header>
+            </div>
+        </div>
+    );
+}
